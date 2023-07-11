62bee2d1f05324967e633fd532b6c22f9c897707
brandonT2002 <3060571900304@ingenieria.usac.edu.gt>
2023-07-10 22:00:36 -0600

    tipo resultante

---
+++ src/Classes/Utils/Operations.java
@@ -0,0 +1,46 @@
+package Classes.Utils;
+
+public class Operations {
+    public static Type[][] plus = {
+            { Type.INT, Type.DOUBLE, Type.INT, Type.INT, Type.STRING },
+            { Type.DOUBLE, Type.DOUBLE, Type.DOUBLE, Type.DOUBLE, Type.STRING },
+            { Type.INT, Type.DOUBLE, Type.NULL, Type.NULL, Type.STRING },
+            { Type.INT, Type.DOUBLE, Type.NULL, Type.STRING, Type.STRING },
+            { Type.STRING, Type.STRING, Type.STRING, Type.STRING, Type.STRING }
+    };
+    public static Type[][] minus = {
+            { Type.INT, Type.DOUBLE, Type.INT, Type.INT, Type.NULL },
+            { Type.DOUBLE, Type.DOUBLE, Type.DOUBLE, Type.DOUBLE, Type.NULL },
+            { Type.INT, Type.DOUBLE, Type.NULL, Type.NULL, Type.NULL },
+            { Type.INT, Type.DOUBLE, Type.NULL, Type.NULL, Type.NULL },
+            { Type.NULL, Type.NULL, Type.NULL, Type.NULL, Type.NULL }
+    };
+    public static Type[][] mult = {
+            { Type.INT, Type.DOUBLE, Type.NULL, Type.INT, Type.NULL },
+            { Type.DOUBLE, Type.DOUBLE, Type.NULL, Type.DOUBLE, Type.NULL },
+            { Type.NULL, Type.NULL, Type.NULL, Type.NULL, Type.NULL },
+            { Type.INT, Type.DOUBLE, Type.NULL, Type.NULL, Type.NULL },
+            { Type.NULL, Type.NULL, Type.NULL, Type.NULL, Type.NULL }
+    };
+    public static Type[][] div = {
+            { Type.DOUBLE, Type.DOUBLE, Type.NULL, Type.DOUBLE, Type.NULL },
+            { Type.DOUBLE, Type.DOUBLE, Type.NULL, Type.DOUBLE, Type.NULL },
+            { Type.NULL, Type.NULL, Type.NULL, Type.NULL, Type.NULL },
+            { Type.DOUBLE, Type.DOUBLE, Type.NULL, Type.NULL, Type.NULL },
+            { Type.NULL, Type.NULL, Type.NULL, Type.NULL, Type.NULL }
+    };
+    public static Type[][] pow = {
+            { Type.INT, Type.DOUBLE, Type.NULL, Type.NULL, Type.NULL },
+            { Type.DOUBLE, Type.DOUBLE, Type.NULL, Type.NULL, Type.NULL },
+            { Type.NULL, Type.NULL, Type.NULL, Type.NULL, Type.NULL },
+            { Type.NULL, Type.NULL, Type.NULL, Type.NULL, Type.NULL },
+            { Type.NULL, Type.NULL, Type.NULL, Type.NULL, Type.NULL }
+    };
+    public static Type[][] mod = {
+            { Type.DOUBLE, Type.DOUBLE, Type.NULL, Type.NULL, Type.NULL },
+            { Type.DOUBLE, Type.DOUBLE, Type.NULL, Type.NULL, Type.NULL },
+            { Type.NULL, Type.NULL, Type.NULL, Type.NULL, Type.NULL },
+            { Type.NULL, Type.NULL, Type.NULL, Type.NULL, Type.NULL },
+            { Type.NULL, Type.NULL, Type.NULL, Type.NULL, Type.NULL }
+    };
+}
